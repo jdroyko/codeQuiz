5d5455ef6d6d04d022e66be5280717f114b84d69
Jeanne <c15jeanne.domingo@gmail.com>
2021-03-10 19:50:41 -0500

    Added more code to store scores.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,30 @@
 
 var score = 0;
 var currQuestion = -1; //current Question
-
+var timer;
 
 //When start button is pressed, timer starts -- This Works!
 /*Acceptance Criteria:GIVEN I am taking a code quiz
 WHEN I click the start button
 THEN a timer starts and I am presented with a question*/
 
-function start(){
+function start() {
 
     timeLeft = 60;
     document.getElementById("timeLeft").innerHTML = timeLeft;
-    timer = setInterval(function() {
+    var timer = setInterval(function () {
         timeLeft--;
         document.getElementById("timeLeft").innerHTML = timeLeft;
         //proceed to end game function when timer runs out
         if (timeLeft <= 0) {
             clearInterval(timer);
-            endGame(); 
+            endGame();
         }
+        // document.getElementById('#btn').style.visibility = 'hidden';
     }, 1000);
 
     next();
-    // document.getElementById('button').style.visibility='hidden';//how to hide the button when the game has started???
+    //how to hide the button when the game has started???
 
 }
 
@@ -33,43 +34,43 @@ function start(){
 
 //random array of questions
 
-var questions = [//write more questions here
+var questions = [
     {
         question: "What does JSON stand for?",
         choices: ["it's a name", "JavaScript Object Notation", "Pass"],
         answer: "JavaScript Object Notation"
-        },
-        {
-         question: "How do you call an id?",
-         choices: [".", "#", "()"],
-         answer: "#"
-        },
-      {
+    },
+    {
+        question: "How do you call an id?",
+        choices: [".", "#", "()"],
+        answer: "#"
+    },
+    {
         question: "What output does a prompt have?",
         choices: ["String", "Number", "Boolean"],
         answer: "String"
-        },
-     {
+    },
+    {
         question: "How do you style an HTML?",
         choices: ["Javascript", "CSS", "JSON"],
         answer: "CSS"
-            },
-      {
+    },
+    {
         question: "What is not a storage option?",
         choices: ["Local", "Session", "Memory"],
         answer: "Memory"
-            },
-      {
+    },
+    {
         question: "What is in quotation marks?",
         choices: ["String", "Number", "Boolean"],
         answer: "String"
-            },
-      {
+    },
+    {
         question: "How do you make an html interactive",
         choices: ["CSS", "JavaScript", "JSON"],
         answer: "Javascript"
-            },
-    
+    },
+
 ];
 
 //Loops through the questions
@@ -79,36 +80,36 @@ THEN I am presented with another question*/
 function next() {
     currQuestion++;
 
-/* Acceptance Criteria: WHEN all questions are answered or the timer reaches 0
-THEN the game is over*/
+    /* Acceptance Criteria: WHEN all questions are answered or the timer reaches 0
+    THEN the game is over*/
 
     if (currQuestion > questions.length - 1) {
         endGame();
         return;
     }
 
-    var quizContent = "<h2>" + questions[currQuestion].question + "</h2>"
+    var content = "<h2>" + questions[currQuestion].question + "</h2>"
 
     for (var buttonLoop = 0; buttonLoop < questions[currQuestion].choices.length; buttonLoop++) {
-        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
+        var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>";
         buttonCode = buttonCode.replace("[CHOICE]", questions[currQuestion].choices[buttonLoop]);
         if (questions[currQuestion].choices[buttonLoop] == questions[currQuestion].answer) {
             buttonCode = buttonCode.replace("[ANS]", "correct()");
         } else {
             buttonCode = buttonCode.replace("[ANS]", "incorrect()");
         }
-        quizContent += buttonCode
+        content += buttonCode
     }
 
 
-    document.getElementById("quiz").innerHTML = quizContent;
+    document.getElementById("quiz").innerHTML = content;
 }
 
 //if player misses a question, time is decreased by 10 seconds -- This Works
 /*WHEN I answer a question incorrectly
 THEN time is subtracted from the clock*/
 function incorrect() {
-    timeLeft -= 10; 
+    timeLeft -= 10;
     next();// calls on the next question
 }
 
@@ -119,20 +120,31 @@ function correct() {
 }
 
 //stores the correct number of scores in the game
+function storeScore() {
+    localStorage.setItem("highScore", score);
+    localStorage.setItem("highScoreName", document.getElementById('name').value);
+    getScore();
+}
 
+function getScore() {
+    var content =
+        localStorage.getItem("highScoreName") + localStorage.getItem("highScore");
+
+    document.getElementById("quiz").innerHTML = content;
+}
 
 
 
 // stops timer to end game .. needs work here
-function endGame (){
+function endGame() {
     clearInterval(timer);
 
-    var quizContent = `
+    var content = `
     <h1>Your Code Quiz Result:</h1>
-    <h3>Your score is ` + score +  `!</h3>
-    <input type="text" id="name" placeholder="First name"> 
+    <h3>Your score is ` + score + `!</h3>
+    <input type="text" id="name" placeholder="name"> 
     <button onclick="saveScore()">Save score!</button>`;
 
-    document.getElementById("quiz").innerHTML = quizContent;
+    document.getElementById("quiz").innerHTML = content;
 }
 
